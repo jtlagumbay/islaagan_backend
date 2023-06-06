91d68363ae7f5ab6f95085ce0084051d5aa4a99c
jtlagumbay <jtlagumbay@up.edu.ph>
2023-06-06 13:28:49 +0800

    fix user login

--- index.js
+++ index.js
@@ -17,6 +17,20 @@ const itRestaurantRouter = require("./api/itRestaurants/itRestaurant.router");
 const itAccommodationRouter = require("./api/itAccommodations/itAccommodation.router");
 const itAquaticRouter = require("./api/itAquatics/itAquatic.router");
 
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+//   );
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "x-access-token, Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
+app.use(cors());
+// app.options("*", cors());
 app.get("/", (req, res) => {
   res.send("ISLAagan Backend 2");
 });
@@ -32,7 +46,6 @@ app.use("/itineraries", checkAPI, itinerariesRouter);
 app.use("/itRestaurants", checkAPI, itRestaurantRouter);
 app.use("/itAccommodations", checkAPI, itAccommodationRouter);
 app.use("/itAquatics", checkAPI, itAquaticRouter);
-app.use(cors());
 
 const port = process.env.APP_PORT || 3000;
 
