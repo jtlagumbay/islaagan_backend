ee8ba60a684d8a619daa17c9337987c7f68ec3e5
jtlagumbay <jtlagumbay@up.edu.ph>
2023-06-11 15:39:32 +0800

    edit update user and remove console logs

--- middlewares/api_validation.js
+++ middlewares/api_validation.js
@@ -1,8 +1,7 @@
 module.exports = {
   checkAPI: (req, res, next) => {
     const apiKey = req.body.api_key; // Assuming the API key is passed in the "x-api-key" header
-    // console.log(req.body);
-    // console.log(process.env.API_KEY);
+
     // Check if the API key is valid
     if (apiKey === process.env.API_KEY) {
       // API key is valid
--- middlewares/token_validation.js
+++ middlewares/token_validation.js
@@ -7,7 +7,6 @@ module.exports = {
       token = token.slice(7);
       verify(token, process.env.JWT_KEY, (err, decoded) => {
         if (err) {
-          console.log(err);
           if (err.name == "TokenExpiredError") {
             return res.status(500).json({
               success: 0,
