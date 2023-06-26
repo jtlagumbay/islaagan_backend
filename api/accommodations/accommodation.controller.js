1158b4b400bbc9080d057d12812eb733726af989
jtlagumbay <jtlagumbay@up.edu.ph>
2023-06-26 21:04:41 +0800

    add recommendations

--- api/accommodations/accommodation.controller.js
+++ api/accommodations/accommodation.controller.js
@@ -2,6 +2,7 @@ const {
   getAllAccommodations,
   getAccommodationById,
   getAccommodationByDestId,
+  getRecoAccommodation,
 } = require("./accommodation.service");
 
 module.exports = {
@@ -78,6 +79,37 @@ module.exports = {
         return res.status(400).json({
           success: 0,
           message: "No accommodation found in destination.",
+        });
+      }
+      return res.status(200).json({
+        success: 1,
+        data: results,
+      });
+    });
+  },
+  getRecoAccommodation: (req, res) => {
+    const type = req.body.type;
+    const price_min = req.body.price_min;
+
+    getRecoAccommodation(type, price_min, (err, results) => {
+      if (err) {
+        console.error(err);
+        if (err.errno == -4078) {
+          return res.status(500).json({
+            success: 0,
+            error: "Database connection error",
+          });
+        } else
+          return res.status(400).json({
+            success: 0,
+            error: err,
+          });
+      }
+      console.log(results);
+      if (results.length < 1) {
+        return res.status(400).json({
+          success: 0,
+          message: "No recommended accommodation found.",
         });
       }
       return res.status(200).json({
--- api/aquatics/aquatic.service.js
+++ api/aquatics/aquatic.service.js
@@ -62,4 +62,31 @@ module.exports = {
       }
     );
   },
+  getRecoAquatic: (type, price_min, callback) => {
+    pool.query(
+      `
+      SELECT t1.*, MAX(t3.image_name) as image_name 
+      FROM (
+        SELECT aqua_id, name, description, type, entrance_fee FROM aquatics WHERE type LIKE CONCAT('%', ?, '%')
+        UNION 
+        SELECT aqua_id, name, description, type, entrance_fee FROM aquatics WHERE entrance_fee>=?
+      ) AS t1
+      LEFT JOIN ( 
+      SELECT category_id, image_name FROM images i where category="aqua" 
+      ) AS t3 ON t1.aqua_id = t3.category_id
+      GROUP BY t1.aqua_id, t1.name, t1.description, t1.entrance_fee, t1.type
+
+      ORDER by rand()  
+      LIMIT 9
+      ;
+`,
+      [type, price_min],
+      (error, results, fields) => {
+        if (error) {
+          return callback(error);
+        }
+        return callback(null, results);
+      }
+    );
+  },
 };
